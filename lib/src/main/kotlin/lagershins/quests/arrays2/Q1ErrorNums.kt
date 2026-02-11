c0093765f1dc42483006eeb52cc50a24c9f53dd5
Marcus Hasslinger <marcus.hasslinger@pm.me>
2026-02-12 09:52:11 +1100

    populate new array to avoid sort runtime: 98.15% memory 97.04%

--- lib/src/main/kotlin/lagershins/quests/arrays2/Q1ErrorNums.kt
+++ lib/src/main/kotlin/lagershins/quests/arrays2/Q1ErrorNums.kt
@@ -2,33 +2,17 @@ package lagershins.quests.arrays2
 
 class Q1ErrorNums {
 	fun findErrorNums(nums: IntArray): IntArray {
-		var missing = 0
 		var duplicate = 0
 
-		nums.sort()
-		for (i in nums.indices) {
-			val (first, second) = nums[i] to nums[i + 1]
-
-			if (missing == 0) {
-				val ef = i + 1 + (if (duplicate == 0) 0 else -1)
-				if (ef != first) {
-					missing = ef
-				}
+		val target = IntArray(nums.size)
+		for (n in nums) {
+			if (target[n - 1] > 0) {
+				duplicate = n
+			} else {
+				target[n - 1] = n
 			}
-
-			if (first == second) {
-				duplicate = first
-			}
-
-			if (missing == 0 && i + 2 == nums.size) {
-				val es = if (first == duplicate) first + 1 else second + 1
-				missing = es
-			}
-
-			if (missing > 0 && duplicate > 0) return intArrayOf(duplicate, missing)
 		}
-
-		// Didn't detect an error but should have!
-		return intArrayOf(0, 0)
+		val missing = target.indexOfFirst { it < 1 } + 1
+		return intArrayOf(duplicate, missing)
 	}
 }
