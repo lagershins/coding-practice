163ffbe5242150a79f6744f0efbae13e1cd97a06
Marcus Hasslinger <marcus.hasslinger@gmail.com>
2026-02-16 09:37:53 +1100

    SQ3 exclusive time of functions

---
+++ leetcode/src/test/kotlin/lagershins/quests/stack/Q3ExclusiveTimeTest.kt
@@ -0,0 +1,29 @@
+package lagershins.quests.stack
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.FieldSource
+
+class Q3ExclusiveTimeTest {
+	companion object {
+		@JvmStatic
+		private val args = listOf(
+			arguments(2, arrayOf("0:start:0", "1:start:2", "1:end:5", "0:end:6"), intArrayOf(3, 4)),
+			arguments(1, arrayOf("0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7"), intArrayOf(8)),
+			arguments(2, arrayOf("0:start:0", "0:start:2", "0:end:5", "1:start:6", "1:end:6", "0:end:7"), intArrayOf(7, 1)),
+			arguments(1, arrayOf("0:start:0", "0:end:2"), intArrayOf(3)),
+			arguments(8, arrayOf("0:start:0","1:start:5","2:start:6","3:start:9","4:start:11","5:start:12","6:start:14","7:start:15","1:start:24","1:end:29","7:end:34","6:end:37","5:end:39","4:end:40","3:end:45","0:start:49","0:end:54","5:start:55","5:end:59","4:start:63","4:end:66","2:start:69","2:end:70","2:start:74","6:start:78","0:start:79","0:end:80","6:end:85","1:start:89","1:end:93","2:end:96","2:end:100","1:end:102","2:start:105","2:end:109","0:end:114"), intArrayOf(20,14,35,7,6,9,10,14)),
+		)
+	}
+
+	val target = Q3ExclusiveTime()
+
+	@ParameterizedTest
+	@FieldSource("args")
+	fun test(n: Int, logs: Array<String>, expected: IntArray) {
+		val l = logs.asList()
+		val e = expected.toTypedArray()
+		assertThat(target.exclusiveTime(n, l)).containsExactly(e)
+	}
+}
