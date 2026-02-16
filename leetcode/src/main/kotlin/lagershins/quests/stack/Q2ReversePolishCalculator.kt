fa07c454cd7d917d75049825275d23b781b6c2d4
Marcus Hasslinger <marcus.hasslinger@gmail.com>
2026-02-16 11:10:04 +1100

    SQ2 add runtime info

--- leetcode/src/main/kotlin/lagershins/quests/stack/Q2ReversePolishCalculator.kt
+++ leetcode/src/main/kotlin/lagershins/quests/stack/Q2ReversePolishCalculator.kt
@@ -3,6 +3,7 @@ package lagershins.quests.stack
 import java.util.Stack
 
 class Q2ReversePolishCalculator {
+	// Runtime: 18ms (68.36%) Memory: 46.19MB (85.27%)
 	fun evaluate(tokens: Array<String>): Int {
 		val ops = mapOf<String, (Int, Int) -> Int>(
 			"+" to Int::plus,
