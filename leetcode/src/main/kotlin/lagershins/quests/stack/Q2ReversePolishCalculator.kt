4738be0730d6178af4736055e3b68503f6b1edd3
Marcus Hasslinger <marcus.hasslinger@gmail.com>
2026-02-16 11:21:53 +1100

    SQ2 create map specifying capacity and loading factor

--- leetcode/src/main/kotlin/lagershins/quests/stack/Q2ReversePolishCalculator.kt
+++ leetcode/src/main/kotlin/lagershins/quests/stack/Q2ReversePolishCalculator.kt
@@ -1,17 +1,16 @@
 package lagershins.quests.stack
 
 import java.util.LinkedList
-import java.util.Stack
 
 class Q2ReversePolishCalculator {
-	// Runtime: 14ms (75.12%) Memory: 46.30MB (82.13%)
+	// Runtime: 11ms (82.61%) Memory: 45.84MB (92.75%)
 	fun evaluate(tokens: Array<String>): Int {
-		val ops = mapOf<String, (Int, Int) -> Int>(
-			"+" to Int::plus,
-			"-" to Int::minus,
-			"*" to Int::times,
-			"/" to Int::div,
-		)
+		val ops = HashMap<String, (Int, Int) -> Int>(5, 1.0f).apply {
+			put("+", Int::plus)
+			put("-", Int::minus)
+			put("*", Int::times)
+			put("/", Int::div)
+		}
 
 		val stack = LinkedList<Int>()
 
